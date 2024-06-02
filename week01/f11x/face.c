4957218d0652984d0ada277ae1d8d5642929e5cc
MysticalFire1315 <joelchong03@gmail.com>
2024-06-02 20:04:08 +1000

    Upload tut01 code

---
+++ week01/f11x/face.c
@@ -0,0 +1,55 @@
+// F11X Tutorial 01 - face.c
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+
+#include <stdio.h>
+
+int main(void) {
+    // Face 1:
+    //  ~ ~
+    //  0 0
+    //   o
+    //   -
+
+    // printf("~ ~\n");
+    // printf("0 0\n");
+    // printf(" o \n");
+    // printf(" - \n");
+
+    // printf("~ ~\n0 0\n o \n - \n");
+
+    printf(
+        "~ ~\n"
+        "0 0\n"
+        " o \n"
+        " - \n"
+    );
+
+    // Face 2:
+    //  ~ ~
+    //  0 0
+    //   o
+    //  \_/
+
+    printf(
+        "~ ~\n"
+        "0 0\n"
+        " o \n"
+        "\\_/\n"
+    );
+
+    return 0;
+}
---
+++ week01/h15b/hello_world.c
@@ -0,0 +1,14 @@
+// H15B Tutorial 01 - hello_world.c
+// Basic Hello, World! program
+
+#include <stdio.h>
+
+int main(void) {
+    // This prints "Hello, World!" to the terminal, and the next terminal
+    // command is printed on the line below it, instead of on the same
+    // line.
+    // What does this imply about what '\n' does?
+    printf("Hello, World!\n");
+
+    return 0;
+}
